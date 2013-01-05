300469a4c459e90cb3cc71e2c36b326b3d78b121
Nathan Sobo <nathan@github.com>
2013-01-05 12:59:48 -0700

    Focus specs by prefixing `it` and `describe` w/ 'f', 'ff'
    This disables the normal browser-based focusing. This approach is a
    lot simpler to control.

---
+++ script/jasmine/lib/jasmine-fspecs.js
@@ -0,0 +1,47 @@
+var setGlobalFocusPriority = function(priority) {
+  env = jasmine.getEnv();
+  if (!env.focusPriority) env.focusPriority = 1;
+  if (priority > env.focusPriority) env.focusPriority = priority;
+};
+
+var fdescribe = function(description, specDefinitions, priority) {
+  if (!priority) priority = 1;
+  setGlobalFocusPriority(priority)
+  var suite = describe(description, specDefinitions);
+  suite.focusPriority = priority;
+  return suite;
+};
+
+var ffdescribe = function(description, specDefinitions) {
+  fdescribe(description, specDefinitions, 2);
+};
+
+var fffdescribe = function(description, specDefinitions) {
+  fdescribe(description, specDefinitions, 3);
+};
+
+var fit = function(description, definition, priority) {
+  if (!priority) priority = 1;
+  setGlobalFocusPriority(priority);
+  var spec = it(description, definition);
+  spec.focusPriority = priority;
+  return spec;
+};
+
+var ffit = function(description, specDefinitions) {
+  fit(description, specDefinitions, 2);
+};
+
+var fffit = function(description, specDefinitions) {
+  fit(description, specDefinitions, 3);
+};
+
+var fSpecFilter = function(specOrSuite) {
+  globalFocusPriority = jasmine.getEnv().focusPriority;
+  if (!globalFocusPriority) return true;
+  if (specOrSuite.focusPriority >= globalFocusPriority) return true;
+
+  var parent = specOrSuite.parentSuite || specOrSuite.suite;
+  if (!parent) return false;
+  return fSpecFilter(parent);
+}
